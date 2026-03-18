cf3abdf2345671e22b55c54ea34bc257189b7fdc
Dave <post@dhold.net>
2026-03-18 09:02:29 +0100

    Changed the script so that it scans every element with the class 'post'. So now it's not restricted to one div with a specific id anymore.

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # OnClickVideo
 
+Deutsch
+-------
 Dieses kleine Skript ersetzt YouTube-Links in einem Text durch Thumbnails. Erst mit einem Klick auf das Thumbnail wird das Video eingebettet.
 
 Das spart eine Menge Daten, die sonst schon beim Seitenaufruf geladen werden. Zudem werden die Cookies durch YouTube auch erst nach dem Klick gesetzt. Für den Nutzer gibt es auch einen entsprechenden Hinweis.
 Allerdings werden die Thumbnails immer noch vorab von YouTube geladen. 
 
 Datenschutzrechtlich ist diese Variante also auch nicht unbedenklich. Aber dafür werden eine Menge Daten gespart.
+
+English
+-------
+
+This small script replaces YouTube links in a text with thumbnails. The video is only embedded once the thumbnail is clicked.
+
+This saves a significant amount of data that would otherwise be loaded when the page is first opened. Additionally, YouTube cookies are only set after the click. A corresponding notice is also provided to the user.
+However, the thumbnails are still preloaded from YouTube.
+
+From a data protection perspective, this variant is therefore not entirely without concern. But on the plus side, it saves a lot of data.
--- onClickVideo.js
+++ onClickVideo.js
@@ -1,37 +1,33 @@
+// onClickVideo
+// Version 1.1
+
+// Autoload when DOM is done loading
+document.addEventListener("DOMContentLoaded", function() {
+    onClickVideo();
+});
+
 function onClickVideo() {
-    var str = document.getElementById("content").innerHTML;
+    var posts = document.querySelectorAll(".post");
 
-    // find every youtube link and replace it
-    var result = str.replace(/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/ig, replaceLink);
-
-    document.getElementById("content").innerHTML = result;
+    posts.forEach(function(postElement) {
+        var str = postElement.innerHTML;
+        var regex = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?‌​=]*)?/ig;
+        var result = str.replace(regex, function(match, videoId) {
+            return createThumbnailHTML(videoId);
+        });
+        postElement.innerHTML = result;
+    });
 }
 
-function replaceLink(item, index) {
-    // get the video id from the link
-    var lMatch = item.match(/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/i);
-
-    // the second entry in the array contains "undefined" if the pattern doesn't match
-    if(lMatch[1] != 'undefined') {
-        var demo = document.getElementById('content');
-
-        // you could also use this line, instead of the next one, if you only want clickable thumbnails
-        //thumbnail = '<a href="https://www.youtube.com/watch?v='+ lMatch[1] +'" target="_blank"><img src="https://img.youtube.com/vi/'+lMatch[1]+ '/hqdefault.jpg"></a>';
-
-        var thumbnail = '<div class="video-container" onclick="embedVideo(this, \''+ lMatch[1] +'\')">'
-        +'<img  width="640" height="360" src="https://img.youtube.com/vi/'+lMatch[1]+ '/hqdefault.jpg">'
-        +'<div class="info-card">Mit einem Klick auf dieses Bild akzeptierst Du, dass Daten von youtube.com geladen werden und youtube.com Cookies in deinem Browser anlegt.</div>'
-        +'<div class="centered">'
-        +'<button class="play-button"></button>'
-        +'</div> </div>';
-
-        return thumbnail;
-    } else {
-        // return original URL if it doesn't match
-        return lMatch[0];
-    }
+function createThumbnailHTML(vId) {
+    return '<div class="video-container" onclick="embedVideo(this, \'' + vId + '\')">'
+        + '<img src="https://img.youtube.com/vi/' + vId + '/hqdefault.jpg">'
+        + '<div class="info-card">Mit einem Klick auf dieses Bild akzeptierst du, dass Daten von youtube.com geladen werden und youtube.com Cookies in deinem Browser anlegt.</div>'
+        + '<div class="centered">'
+        + '<button class="play-button"></button>'
+        + '</div></div>';
 }
 
 function embedVideo(element, v) {
-    element.innerHTML = '<iframe width="640" height="360" src="https://www.youtube.com/embed/'+ v +'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    element.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ v +'?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
